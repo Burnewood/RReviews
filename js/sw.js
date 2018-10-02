094c17567416c87239c48d8f9d6e7fb1b00779b2
Patrick Woodburne <p.wood@live.ca>
2018-10-01 20:29:17 -0400

    Feat: add listener for fetch event

--- js/sw.js
+++ js/sw.js
@@ -31,3 +31,29 @@ self.addEventListener('install',function(e){
     })
   );
 });
+
+// listen for fetch event
+self.addEventListener('fetch',function(e){
+  e.respondWith(
+    caches.match(e.request).then(function(response){
+      if(response){
+        console.log('found ',e.request,' in cache');
+        return response;
+      }
+      else{
+        console.log('could not find ',e.request,' in cache, fetching...');
+        return fetch(e.request)
+        .then(function(response){
+          const clonedResponse = response.clone();
+          caches.open('v1').then(function(cache){
+            cache.put(e.request,response);
+          })
+          return response;
+        })
+        .catch(function(err){
+          console.error(err);
+        });
+      }
+    })
+  );
+});
